adf1bc8ca1bb30173bcae2083baf0f628532c831
Nasadude <mike@technating.io>
2025-12-02 14:41:21 -0600

    Added endpoints with FastAPI

--- predictor.py
+++ predictor.py
@@ -8,6 +8,20 @@ def load_model(model_path):
 
 # Make predictions based on this loaded model
 def make_predictions(model, input_features):
+
+    '''
+    Make predictions using a loaded model and input features.
+
+    Parameters:
+    - model (object): The pre-trained model.
+    - input_features (List[List[float]]): Input features for making predictions.
+
+    Returns:
+    - Tuple[str, List[float], List[str]]: A tuple containing the predicted class,
+    probabilities, and classes.
+    '''
+
+
     predict_class = model.predict(input_features)[0]
     probabilities = model.predict_proba(input_features)[0]
     classes = model.classes_
